097bec69b6a993955a24b88558f262b9a32f773a
Beltran Rueda <beltran@bitnami.com>
2018-02-07 13:41:03 +0000

    Folder rename

--- java-singlevm/app/boot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-#Configure JAVA_OPTS
-echo "JAVA_OPTS=\"-Djava.awt.headless=true -Xmx512m\"" >> /usr/share/tomcat/conf/tomcat.conf
-
-exec "$@"
--- java-singlevm/app/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Extra packages
-yum install -y mysql && yum clean all && rm -fR /var/cache/yum
-
-echo "=== FINISH BUILD SCRIPT ==="
-
